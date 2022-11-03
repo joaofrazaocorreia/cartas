42e193095fa91bcd1a309ec13d5f0bb2dbc60b88
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2022-11-03 14:16:06 +0000

    first commit

---
+++ cartas.py
@@ -0,0 +1,16 @@
+import random
+deck1=[1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+deck2=[1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+deck1=random.shuffle(Deck1)
+deck2=random.shuffle(Deck2)
+
+game=True
+
+while game:
+
+    print("PLAYER vs COMP")
+    print("")
+
+    
+
+
